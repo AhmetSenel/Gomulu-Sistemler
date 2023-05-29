0911e19dd9da23c59d83529d55d11d34582a42b7
Ahmet Şenel <senelahmet1414@gmail.com>
2023-05-29 18:25:32 +0300

    turnike

--- Turnike/src/turnike.v
+++ Turnike/src/turnike.v
@@ -12,14 +12,14 @@ reg [31:0] counter3;
 always @(posedge clk) begin
     if (!btn1)
         counter1 <= 32'd0;
-    else if (counter1 < 32'd100_000_000)
+    else if (counter1 < 32'd120_000_000)
         counter1 <= counter1 + 1;
 end
 
 always @(posedge clk) begin
     if (!btn2)
         counter2 <= 32'd0;
-    else if (counter2 < 32'd100_000_000)
+    else if (counter2 < 32'd120_000_000)
         counter2 <= counter2 + 1;
 end
 
@@ -31,14 +31,14 @@ always @(posedge clk) begin
 end
 
 always @(posedge clk) begin
-    if (counter1 < 32'd100_000_000)
-        led <= 3'b011;
-    else if (counter2 < 32'd100_000_000)
+    if (counter1 < 32'd120_000_000)         //5 saniye yeşil
+        led <= 3'b011;                         
+    else if (counter2 < 32'd120_000_000)    //5 saniye kırmızı
         led <= 3'b110;
-    else if (counter3 < 32'd24_000_000)
+    else if (counter3 < 32'd24_000_000)     //1 saniye mavi
         led <= 3'b101;
-    else 
-        led <= 3'b111;
+    else                                    //1 saniye ledleri kapat
+        led <= 3'b111;                      
 end
 
 endmodule
